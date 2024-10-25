039dae780c9621f7eaa57c99b478dabe0a9ef121
Artudevp <abdielarturoart@gmail.com>
2024-10-24 19:07:52 -0500

    App Ecotourexpress

---
+++ app/src/main/java/com/ecotourexpress/ecotourexpress/ActivitiesActivity.java
@@ -0,0 +1,13 @@
+package com.ecotourexpress.ecotourexpress;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivitiesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activities);
+    }
+}
---
+++ app/src/main/java/com/ecotourexpress/ecotourexpress/ClientsActivity.java
@@ -0,0 +1,13 @@
+package com.ecotourexpress.ecotourexpress;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ClientsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clients);
+    }
+}
